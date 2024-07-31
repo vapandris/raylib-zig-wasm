3102585ce49d15c8825127f6bf623e9ae575bcf7
vapandris <vapandris@gmail.com>
2024-07-31 18:19:06 +0200

    Uploaded template ZIG 0.12 project to git

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # raylib-zig-wasm
-ZIG raylib wasm example. Zig version: 0.12
+A really simple example and a guide on how to make raylib-zig work with wasm
+
+Zig version is 0.12 for stability
+
+## Dependencies:
+- zig version 0.12: https://ziglang.org/
+- raylib-zig (branch 0.12): https://github.com/Not-Nik/raylib-zig/tree/zig-0.12
+- emsdk: https://emscripten.org/
+- python 3.6 (or newer)
+
+## Steps to take for wasm compilation:
+- make sure that the hash is correct in build.zig.zon (can check with zig build)
+- run: zig build -Dtarget=wasm32-emscripten --sysroot \<emsdk-location\>/upstream/emscripten
+- can check output in: zig-out/htmlout/
+- start localhost server with: python -m https.server
+- open http://localhost:8000/
+- open zig-out/htmlout/ & profit
---
+++ build.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const emcc = @import("emcc.zig");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    
+    const raylib_dep = b.dependency("raylib-zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib");
+    const raylib_math = raylib_dep.module("raylib-math");
+    const raylib_artifact = raylib_dep.artifact("raylib");
+
+    //web exports are completely separate
+    if (target.query.os_tag == .emscripten) {
+        const exe_lib = emcc.compileForEmscripten(b, "Project", "src/main.zig", target, optimize);
+
+        exe_lib.linkLibrary(raylib_artifact);
+        exe_lib.root_module.addImport("raylib", raylib);
+        exe_lib.root_module.addImport("raylib-math", raylib_math);
+
+        // Note that raylib itself is not actually added to the exe_lib output file, so it also needs to be linked with emscripten.
+        const link_step = try emcc.linkWithEmscripten(b, &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact });
+
+        b.getInstallStep().dependOn(&link_step.step);
+        const run_step = try emcc.emscriptenRunStep(b);
+        run_step.step.dependOn(&link_step.step);
+        const run_option = b.step("run", "Run Project");
+        run_option.dependOn(&run_step.step);
+        return;
+    }
+
+    const exe = b.addExecutable(.{ .name = "Project", .root_source_file = .{ .path = "src/main.zig" }, .optimize = optimize, .target = target });
+
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raylib-math", raylib_math);
+
+    const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run Project");
+    run_step.dependOn(&run_cmd.step);
+
+    b.installArtifact(exe);
+}
+
